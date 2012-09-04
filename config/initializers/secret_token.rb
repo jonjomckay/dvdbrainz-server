43248b6def79a83c3413234a27b45df1c9bc0e6a
Jonjo McKay <jonjo@jonjomckay.com>
2012-09-04 22:35:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DVDBrainz::Application.config.secret_token = '24cae786d25812263a92ddda8be1f475579640974a7b6d120d94a672f0e31aff89fc7ee46cb595a5e0b8e4a2c9bafcbf0dcaaca0bd33a4391862ffdf92244cf4'
